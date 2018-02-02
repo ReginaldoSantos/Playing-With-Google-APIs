135526b8b456208a7904a6054df7c31eb9696b79
ReginaldoSantos <reginaldo_email@yahoo.com.br>
2018-02-02 17:46:57 -0200

    First download test

--- googleapis-drive-api-client/src/main/java/Quickstart.java
+++ googleapis-drive-api-client/src/main/java/Quickstart.java
@@ -1,3 +1,11 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -5,19 +13,13 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.*;
 import com.google.api.services.drive.Drive;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 public class Quickstart {
     /** Application name. */
@@ -44,7 +46,7 @@ public class Quickstart {
      * at ~/.credentials/drive-java-quickstart
      */
     private static final List<String> SCOPES =
-        Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+        Arrays.asList(DriveScopes.DRIVE);
 
     static {
         try {
@@ -113,6 +115,17 @@ public class Quickstart {
                 System.out.printf("%s (%s)\n", file.getName(), file.getId());
             }
         }
+        
+        String fileId = "1ZU2VdbRIof9A6g-unET5C5ZpPjEieOvFkUbEXJW2CVQ";
+        
+        java.io.File fileDownload = new java.io.File("/home/reginaldosantos/Downloads/test.xls");
+        
+        //OutputStream outputStream = new ByteArrayOutputStream();
+        OutputStream outputStream = new FileOutputStream(fileDownload);
+        service
+          .files()
+          .export(fileId, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+          .executeMediaAndDownloadTo(outputStream);
     }
 
 }
